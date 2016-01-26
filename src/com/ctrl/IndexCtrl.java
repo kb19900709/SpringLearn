dd844af843fbb3f11d62f48fe5e4ebedf81e74ad
KB <o922431962@yahoo.com.tw>
2016-01-26 23:56:22 +0800

    新增部分註解

--- src/com/ctrl/IndexCtrl.java
+++ src/com/ctrl/IndexCtrl.java
@@ -10,8 +10,18 @@ public class IndexCtrl {
 	
 	private static final String PRATICE_VIEW = "springPractice";
 	
+	//可攜帶額外參數的寫法
+//	@RequestMapping(value = "/forwardToPracticePage")
+//	public ModelAndView forwardToPracticePage(){
+//		ModelAndView mav = new ModelAndView(PRATICE_VIEW);
+//		mav.addObject("KB", new Date());
+//		return mav;
+//	}
+	
+	//單純回傳邏輯名稱，將會被 InternalResourceViewResolver 解析成對應路徑(view)
+	//接著再由DispatcherServlet 派發
 	@RequestMapping(value = "/forwardToPracticePage")
-	public ModelAndView forwardToPracticePage(){
-		return new ModelAndView(PRATICE_VIEW);
+	public String forwardToPracticePage(){
+		return PRATICE_VIEW;
 	}
 }
